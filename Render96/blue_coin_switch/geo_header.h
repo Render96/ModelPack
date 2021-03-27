ed3ce515726a8caa76c3185703b5893e8a082820
DorfDork <nickfgh@gmail.com>
2021-03-27 15:02:10 -0500

    Dynos files for the latest version and goddard head

--- Render96/blue_coin_switch/geo_header.h
+++
@@ -1,9 +0,0 @@
-extern const GeoLayout blue_coin_switch_geo[];
-extern Vtx blue_coin_switch_000_displaylist_mesh_vtx_0[168];
-extern Gfx blue_coin_switch_000_displaylist_mesh_tri_0[];
-extern Vtx blue_coin_switch_000_displaylist_mesh_vtx_1[395];
-extern Gfx blue_coin_switch_000_displaylist_mesh_tri_1[];
-
-extern Gfx blue_coin_switch_000_displaylist_mesh[];
-extern Gfx blue_coin_switch_material_revert_render_settings[];
-
