59d76999b5bc038d76d652a5480b75f2e9444bfa
DorfDork <nickfgh@gmail.com>
2021-05-21 20:54:36 -0500

    archives only

--- Render96_Alpha_v3.0/actors/spiny_egg/geo_header.h
+++
@@ -1,9 +0,0 @@
-extern const GeoLayout spiny_egg_geo[];
-extern Vtx spiny_egg_root_mesh_vtx_0[181];
-extern Gfx spiny_egg_root_mesh_tri_0[];
-extern Vtx spiny_egg_root_mesh_vtx_1[218];
-extern Gfx spiny_egg_root_mesh_tri_1[];
-
-extern Gfx spiny_egg_root_mesh[];
-extern Gfx spiny_egg_material_revert_render_settings[];
-
--- Render96_Alpha_v3.0/actors/transparent_star/geo_header.h
+++
@@ -1,7 +0,0 @@
-extern const GeoLayout transparent_star_geo[];
-extern Vtx transparent_star_000_displaylist_mesh_vtx_0[159];
-extern Gfx transparent_star_000_displaylist_mesh_tri_0[];
-
-extern Gfx transparent_star_000_displaylist_mesh[];
-extern Gfx transparent_star_material_revert_render_settings[];
-
